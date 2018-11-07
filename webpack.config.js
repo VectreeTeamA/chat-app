1da841ac5984e6865d8914d127e555fbd4d387aa
Polina Shneider <beatrixlinkoln@gmail.com>
2018-11-07 20:22:22 +0300

    Change project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,46 @@
-const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const config = {
-    entry: './src/app.js',
+const outputDirectory = 'dist';
+
+module.exports = {
+    entry: './src/client/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, outputDirectory),
         filename: 'bundle.js'
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
-                use: [
-                    /** Babel loader */
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                "@babel/preset-env",
-                                "@babel/preset-react"
-                            ]
-                        }
-                    }
-                ]
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
-    }
+    },
+    devServer: {
+        port: 3001,
+        open: true,
+        proxy: {
+            '/api': 'http://localhost:8088'
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin([outputDirectory]),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico'
+        })
+    ]
 };
-module.exports = config;
